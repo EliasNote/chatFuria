206fed84fdfe54ead001c204b933c2c3921a32ce
Elias Mathias Sand <elias.coder1@gmail.com>
2025-04-24 11:07:34 -0300

    feat: configuração do controller

--- chat-api/src/app.controller.ts
+++ chat-api/src/app.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('chat')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post()
+  async pesquisar(
+    @Body('prompt') prompt: string,
+  ): Promise<{ response: string }> {
+    return { response: await this.appService.pesquisar(prompt) };
   }
 }
