9a8c9f934a1f9889653274457533475a1d622145
Elias Mathias Sand <elias.coder1@gmail.com>
2025-04-24 11:10:44 -0300

    feat: Configuração do service

--- chat-api/src/app.service.ts
+++ chat-api/src/app.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { GoogleGenAI } from '@google/genai';
+import { FURIA_INITIAL_PROMPT } from './prompt/prompt';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private ai: GoogleGenAI;
+
+  constructor() {
+    this.ai = new GoogleGenAI({
+      apiKey: process.env.GEMINI_API_KEY!,
+    });
+  }
+
+  async pesquisar(prompt: string): Promise<string> {
+    const response = await this.ai.models.generateContent({
+      model: process.env.GEMINI_API_MODEL!,
+      contents: [FURIA_INITIAL_PROMPT, `Pergunta: ${prompt}`],
+      config: {
+        tools: [{ googleSearch: {} }],
+      },
+    });
+    return response.text!;
   }
 }
