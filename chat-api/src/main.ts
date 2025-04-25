d3be1444e1ddf4f03170fc8a6228b37f3f847cd4
Elias Mathias Sand <elias.coder1@gmail.com>
2025-04-25 10:39:26 -0300

    feat: Habilitação dos CORS

--- chat-api/src/main.ts
+++ chat-api/src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
+  });
+
   app.setGlobalPrefix('api/v1');
   await app.listen(process.env.PORT ?? 8080);
 }
